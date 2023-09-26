7036359676f5ee93478bd81038dc973a074cf197
Lodha <lodhar@lri-107201.cc.ad.cchs.net>
2023-09-26 13:54:54 -0400

    updated dicom2nii.py file for server use

--- dicom2nii.py
+++ dicom2nii.py
@@ -1,7 +1,9 @@
+import os
+import dicom2nifti
 import dicom2nifti.settings as settings
-settings. disable_validate_slice_increment()
+settings.disable_validate_slice_increment()
 
-for root, sub, files in os.walk('/home/gonoush/mmm_data/'):
+for root, sub, files in os.walk('/proj/genetics/Projects/shared/Subject Sources/External/Vanderbilt_CT_fromCWRU/Data/'):
     if not sub and files:
         if len(files)>60:
             print(root)
@@ -9,10 +11,10 @@ for root, sub, files in os.walk('/home/gonoush/mmm_data/'):
             print(code)
             subcode=root[-5::]
             print(subcode)
-            new_path='/media/gonoush/Elements/data/Van_nii/'+code
+            new_path='/home/lodhar/vandynifti/'+code
             if not os.path.exists(new_path):
                 os.mkdir(new_path)
-            new_path='/media/gonoush/Elements/data/Van_nii/'+code+'/'+subcode
+            new_path='/home/lodhar/vandynifti/'+code+'/'+subcode
             if not os.path.exists(new_path):
                 os.mkdir(new_path)
             dicom2nifti.convert_directory(root, new_path)
